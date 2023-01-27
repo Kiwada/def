697366a6b2623b860db9429b15d930c7842a0c03
Kaio Fontenele <82303943+Kiwada@users.noreply.github.com>
2023-01-27 11:44:26 -0300

    Add files via upload

---
+++ ex96.py
@@ -0,0 +1,30 @@
+#ex96
+#Faça um Programa que tenha uma função chamada Area() de um terreno retangular (largura e comprimento) e mostre a área do terreno.
+def lin():
+    print('Controle de Terrenos')
+    print('-=-'*10)
+
+def área(L,C):
+    A = L*C
+    print(f'A àrea de um terreno {L} x {C} é de {A} ')
+
+
+#Programa Principal
+lin()
+L = float(input('Largura(m)'))
+C = float(input('Comprimento(m):'))
+área(L,C)
+
+
+
+
+#def area(larg, comp):
+#    a = larg * comp
+#    print(f'área de um terreno {larg} x{comp} é de {a}m2.')
+
+#programa principal
+#   print('controle de terrenos')
+#   print('-' *30)
+# l = float(input('Largura(M):))
+# c = float(input('COMRPIMENTO(M):))
+# area(l,c)
